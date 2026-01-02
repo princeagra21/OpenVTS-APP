b4990d4ad77de03b53fbda87764c5223987114e1
Sanieyne <alhajine20@gmail.com>
2026-01-02 12:12:18 +0100

    adding some components

---
+++ lib/shared/components/custom_text_field.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData prefixIcon;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final double fontSize;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.prefixIcon,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    required this.fontSize,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return TextField(
+      controller: widget.controller,
+      obscureText: widget.isPassword ? _obscureText : false,
+      keyboardType: widget.keyboardType,
+      style: GoogleFonts.inter(fontSize: widget.fontSize, color: colorScheme.onSurface),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.transparent,
+        hintText: widget.hintText,
+        hintStyle: GoogleFonts.inter(
+          color: colorScheme.onSurface.withOpacity(0.5),
+          fontSize: widget.fontSize,
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        prefixIconConstraints: const BoxConstraints(minWidth: 48),
+        prefixIcon: Icon(widget.prefixIcon, color: colorScheme.primary, size: 22),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off_outlined : Icons.visibility_outlined,
+                  color: colorScheme.primary,
+                ),
+                onPressed: () => setState(() => _obscureText = !_obscureText),
+              )
+            : null,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: colorScheme.primary.withOpacity(0.1)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: colorScheme.primary.withOpacity(0.1)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: colorScheme.primary, width: 1.5),
+        ),
+      ),
+    );
+  }
+}
